9e7d49160a48ebed19f9722a97b1772633d39c52
yashwanthreddy710 <yeshwanthreddy216@gmail.com>
2025-07-18 00:51:52 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import contactRoutes from './routes/contactRoutes.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/contact', contactRoutes);
+
+// DB Connect
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    );
+  })
+  .catch((err) => {
+    console.error('DB connection error:', err.message);
+  });
